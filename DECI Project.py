a26e5d411c5268a6142531bfb5b95edaaff0e038
BaselArafa <110883356+BaselArafa@users.noreply.github.com>
2023-06-05 00:04:38 +0300

    Update DECI Project.py

--- DECI Project.py
+++ DECI Project.py
@@ -1,90 +1,133 @@
-ts = 0
 import time
 import random
-cong = ['you got it !!', 'congratulations', 'wow !! you won']
-los = ['oops you lost ..', 'game over', 'Unluckly . try again later ']
+#track the score
+ts = 0
+#import time and random to hundle the game
+cong = ["you got it !!", "congratulations",\
+    "wow !! you won"]
+los = ["oops you lost ..", "game over",\
+     "Unluckly . try again later "]
 win = random.choice(cong)
 lose =  random.choice(los)
+#code the game in a def function to be
+#able to call a new game easly
+
+
 def main():
-    print('while on a walk in the woods , at a dark night ...')
     time.sleep(2)
-    print("a strange man with a tall magic wood is calling you.")
+    #first scene
+    print("while on a walk in the woods \
+, at a dark night ...")
     time.sleep(2)
-    print("you stumbled upon a secret pathway you had never seen before")
+    print("a strange man with a tall \
+magic wood is calling you.")
+    time.sleep(2)
+    print("you stumbled upon a secret \
+pathway you had never seen before")
     time.sleep(2)
     print("Total score = " + str(ts) )
     print("Enter 1 to follow the pathway. ")
     print("Enter 2 to respond to the strange man.")
-    ans = int(input("(please enter 1 or 2)"))
-    while ans != 1 and ans != 2 :
-        ans = int(input("(please enter 1 or 2)"))
+
+    ans = input("(please enter 1 or 2)")
+    #check the answer
+    while ans != "1" and ans != "2" :
+        ans = input("(please enter 1 or 2)")
+
+
     def pathway ():
-        if ans == 1 :
+        if ans == "1" :
             ts = 5
             time.sleep(2)
-            print(" Curious, you followed the path and found yourself in front of an old, abandoned mansion.")
+            print(" Curious, you followed the \
+path and found yourself in front of \
+an old, abandoned mansion.")
             time.sleep(2)
-            print("you felt a strange pull towards the mansion and decided to investigate further.")
+            print("you felt a strange pull towards \
+the mansion and decided to investigate further.")
             time.sleep(1)
             print("Total score = " + str(ts) )
             print("Enter 1 to continue ")
             print("Enter 2 to run away ")
+
+
     def man ():
-        if ans == 2 :
+        if ans == "2" :
             ts =  5
             time.sleep(2)
             print("you followed the strange man")
             time.sleep(2)
-            print("the man: Hi! I am Karl , let's play with me hahaha!!")
+            print("the man: Hi! I am Karl \
+, let's play with me hahaha!!")
             time.sleep(1)
             print("Total score = " + str(ts) )
             print("Enter 1 to follow him ")
             print("Enter 2 to run away ")
+
     pathway()
     man()
-    ans2 = int(input("(please enter 1 or 2)"))
-    while ans2 != 1 and ans2 != 2 :
-        ans2 = int(input("(please enter 1 or 2)"))
+
+    ans2 = input("(please enter 1 or 2)")
+    #check the answer
+    while ans2 != "1" and ans2 != "2" :
+        ans2 = input("(please enter 1 or 2)")
+
+
     def Continue ():
-        if ans== 1 and ans2 == 1:
+        if ans== "1" and ans2 == "1":
             ts =  10
             time.sleep(2)
-            print("you slowly pushed open the creaky door and entered the dark and dusty house.")
+            print("you slowly pushed open \
+the creaky door and entered \
+the dark and dusty house.")
             time.sleep(2)
-            print("you noticed that every room was filled with books.")
-            print("the mansion is a library that had been abandoned for years.")
+            print("you noticed that every \
+room was filled with books.")
+            print("the mansion is a library \
+that had been abandoned for years.")
             time.sleep(2)
-            print("there are two books on the table . blue and red one")
+            print("there are two books on \
+the table . blue and red one")
             time.sleep(1)
             print("Total score = " + str(ts) )
             print("Enter 1 to open the blue book ")
             print("Enter 2 to open the red book ")
+
+
     def runaway ():
-        if ans== 1 and ans2 == 2:
+        if ans== "1" and ans2 == "2":
             ts = 0
             time.sleep(2)
             print("the strange man was following you !")
             time.sleep(2)
-            print("You became a frog using his magic wood !!!!")
+            print("You became a frog using \
+his magic wood !!!!")
             print(lose)
             print("Total score = " + str(ts) )
+            #replay function
+
             replay = input("Play again ?  y/n ")
             while str(replay)!="y" and str(replay)!="n":
                 replay = input("Play again ?  y/n ")
             if str(replay)== "y" :
                 main()
+            #end game function
             elif str(replay)== "n" :
                 print("see you later .")
                 time.sleep(2)
                 exit()
+
+
     def follow ():
-        if ans== 2 and ans2 == 1:
+        if ans== "2" and ans2 == "1":
             ts = 0
             time.sleep(2)
-            print("You became a frog using his magic wood !!!!")
+            print("You became a frog using \
+his magic wood !!!!")
             time.sleep(2)
             print(lose)
             print("Total score = " + str(ts) )
+
             replay = input("Play again ?  y/n ")
             while str(replay)!="y" and str(replay)!="n":
                 replay = input("Play again ?  y/n ")
@@ -94,54 +137,73 @@ def main():
                 print("see you later .")
                 time.sleep(2)
                 exit()
+
+
     def Runaway():
-        if ans== 2 and ans2 == 2:
+        if ans== "2" and ans2 == "2":
             ts =  10
             time.sleep(2)
-            print("you passed the secret pathway and found yourself in front of a ...")
+            print("you passed the secret pathway \
+and found yourself in front of a ...")
             time.sleep(2)
             print("dark and dusty house.")
             time.sleep(1)
             print("Total score = " + str(ts) )
             print("Enter 1 to Enter the house")
             print("Enter 2 to come back")
+
     Continue()
     runaway()
     follow()
     Runaway()
-    ans3 = int(input("(please enter 1 or 2)"))
-    while ans3 != 1 and ans3 != 2 :
-        ans3 = int(input("(please enter 1 or 2)"))
+
+    ans3 = input("(please enter 1 or 2)")
+    #check the answer
+    while ans3 != "1" and ans3 != "2" :
+        ans3 = input("(please enter 1 or 2)")
+
+
     def blue ():
-        if ans== 1 and ans2 == 1 and ans3 ==1 :
+        if ans== "1" and ans2 == "1"\
+           and ans3 =="1" :
             ts = 5
             time.sleep(2)
-            print("The blue book is fake , it was a box!!")
+            print("The blue book is fake \
+, it was a box!!")
             time.sleep(2)
             print("Total score = " + str(ts) )
             time.sleep(1)
             print("Enter 1 to open the box")
             print("Enter 2 to open the red book ")
+
+
     def red ():
-        if ans== 1 and ans2 == 1 and ans3 ==2 :
+        if ans== "1" and ans2 == "1"\
+           and ans3 == "2" :
             ts = 15
             time.sleep(2)
-            print("The red book is guiding you to the second room !! ")
+            print("The red book is guiding \
+you to the second room !! ")
             time.sleep(2)
             print("Total score = " + str(ts) )
             time.sleep(1)
             print("Enter 1 to go to the room")
             print("Enter 2 to run away ")
+
+
     def enter ():
-        if ans== 2 and ans2 == 2 and ans3==1 :
+        if ans== "2" and\
+           ans2 == "2" and ans3=="1" :
             ts = 0
             time.sleep(2)
             print("the house is very dark !!")
             time.sleep(2)
-            print("A ghost came out of one of the rooms and killed you!!!!")
+            print("A ghost came out of one \
+of the rooms and killed you!!!!")
             time.sleep(2)
             print(lose)
             print("Total score = " + str(ts) )
+
             replay = input("Play again ?  y/n ")
             while str(replay)!="y" and str(replay)!="n":
                 replay = input("Play again ?  y/n ")
@@ -151,14 +213,19 @@ def main():
                 print("see you later .")
                 time.sleep(2)
                 exit()
+
+
     def come():
-        if ans== 2 and ans2 == 2 and ans3==2 :
+        if ans== "2" and \
+           ans2 == "2" and ans3=='2' :
             ts = 0
             time.sleep(2)
-            print("You became a frog using his magic wood !!!!")
+            print("You became a frog using\
+his magic wood !!!!")
             time.sleep(2)
             print(lose)
             print("Total score = " + str(ts) )
+
             replay = input("Play again ?  y/n ")
             while str(replay)!="y" and str(replay)!="n":
                 replay = input("Play again ?  y/n ")
@@ -168,21 +235,30 @@ def main():
                 print("see you later .")
                 time.sleep(2)
                 exit()
+
     blue ()
     red()
     enter()
     come()
-    ans4 = int(input('(Please enter 1 or 2)'))
-    while ans4 != 1 and ans4 != 2 :
-        ans4 = int(input("(please enter 1 or 2)"))
+
+    ans4 = input("(Please enter 1 or 2)")
+    #check the answer
+    while ans4 != "1" and ans4 != "2" :
+        ans4 = input("(please enter 1 or 2)")
+
+
     def box() :
-        if ans== 1 and ans2 == 1 and ans3 ==1 and ans4 ==1 :
+        if ans== "1" and \
+           ans2 == "1" and \
+           ans3 =="1" and ans4 =="1" :
             ts = 0
             time.sleep(2)
-            print("A snake got out of the box and killed you !!")
+            print("A snake got out of \
+the box and killed you !!")
             time.sleep(2)
             print(lose)
             print("Total score = " + str(ts) )
+
             replay = input("Play again ?  y/n ")
             while str(replay)!="y" and str(replay)!="n":
                 replay = input("Play again ?  y/n ")
@@ -192,24 +268,34 @@ def main():
                 print("see you later .")
                 time.sleep(2)
                 exit()
+
+
     def Red ():
-        if ans== 1 and ans2 == 1 and ans3 ==1 and ans4 ==2 :
+        if ans== "1" and\
+           ans2 == "1" and\
+           ans3 =="1" and ans4 =="2" :
             ts = 10
             time.sleep(2)
-            print("The red book is guiding you to the second room !! ")
+            print("The red book is guiding \
+you to the second room !! ")
             time.sleep(2)
             print("Total score = " + str(ts) )
             time.sleep(1)
             print("Enter 1 to go to the room")
             print("Enter 2 to run away ")
+
+    
     def room () :
-        if ans== 1 and ans2 == 1 and ans3 ==2 and ans4==1 :
+        if ans== "1" and \
+           ans2 == "1" and \
+           ans3 =="2" and ans4=="1" :
             ts = 20
             time.sleep(2)
             print ("You got a treasure in the room !!")
             time.sleep(1)
             print(win)
             print("Total score = " + str(ts) )
+    
             replay = input("Play again ?  y/n ")
             while str(replay)!="y" and str(replay)!="n":
                 replay = input("Play again ?  y/n ")
@@ -219,17 +305,24 @@ def main():
                 print("see you later .")
                 time.sleep(2)
                 exit()
+
+    
     def RUN() :
-        if ans== 1 and ans2 == 1 and ans3 ==2 and ans4==2 :
+        if ans== "1" and ans2 == "1" \
+           and ans3 =="2" and ans4=="2" :
             ts = 0
             time.sleep(2)
-            print("the strange man was following you !")
+            print("the strange man was \
+following you !")
             time.sleep(2)
-            print("You became a frog using his magic wood !!!!")
+            print("You became a frog using \
+his magic wood !!!!")
             print(lose)
             print("Total score = " + str(ts) )
+    
             replay = input("Play again ?  y/n ")
-            while str(replay)!="y" and str(replay)!="n":
+            while str(replay)!="y" and \
+                  str(replay)!="n":
                 replay = input("Play again ?  y/n ")
             if str(replay)== "y" :
                 main()
@@ -237,23 +330,34 @@ def main():
                 print("see you later .")
                 time.sleep(2)
                 exit()
+
     RUN()
     room()
     box()
     Red()
-    ans5 = int(input('(Please enter 1 or 2)'))
-    while ans5 != 1 and ans5 != 2 :
-        ans5 = int(input("(please enter 1 or 2)"))
+    
+    ans5 = input("(please enter 1 or 2)")
+    #check the answer
+    while ans5 != "1" and ans5 != "2" :
+        ans5 = input("(please enter 1 or 2)")
+
+
     def GR ():
-        if ans== 1 and ans2 == 1 and ans3 ==1 and ans4 ==2 and ans5 ==1:
+        if ans== "1" and \
+           ans2 == "1" and\
+           ans3 =="1" and\
+           ans4 =="2" and ans5 =="1":
             ts=15
             time.sleep(2)
-            print ("You got a treasure in the room !!")
+            print ("You got a treasure\
+in the room !!")
             time.sleep(1)
             print(win)
             print("Total score = " + str(ts) )
+    
             replay = input("Play again ?  y/n ")
-            while str(replay)!="y" and str(replay)!="n":
+            while str(replay)!="y" and\
+                  str(replay)!="n":
                 replay = input("Play again ?  y/n ")
             if str(replay)== "y" :
                 main()
@@ -261,17 +365,25 @@ def main():
                 print("see you later .")
                 time.sleep(2)
                 exit()
+
+   
     def RA():
-        if ans== 1 and ans2 == 1 and ans3 ==1 and ans4 ==2 and ans5 ==2:
+        if ans== "1" and \
+           ans2 == "1" and \
+           ans3 =="1" and\
+           ans4 =="2" and ans5 =="2":
             ts=0
             time.sleep(2)
-            print("the strange man was following you !")
+            print("the strange man was \
+following you !")
             time.sleep(2)
-            print("You became a frog using his magic wood !!!!")
+            print("You became a frog using \
+his magic wood !!!!")
             print(lose)
             print("Total score = " + str(ts) )
             replay = input("Play again ?  y/n ")
-            while str(replay)!="y" and str(replay)!="n":
+            while str(replay)!="y" and\
+                  str(replay)!="n":
                 replay = input("Play again ?  y/n ")
             if str(replay)== "y" :
                 main()
@@ -279,6 +391,9 @@ def main():
                 print("see you later .")
                 time.sleep(2)
                 exit()
+
     GR()
     RA()
+#run the game
+
 main()
